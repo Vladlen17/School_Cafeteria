d67b9c3a1f7385b5f4ba5bab1e23a657c050c72a
Vladlen <vladlencherkashin2004@gmail.com>
2024-03-19 02:14:01 +0700

    Рабочая авторизация

---
+++ Data.Models/Data/DatabaseContext.cs
@@ -0,0 +1,21 @@
+﻿using Data.Models.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data.Models.Data
+{
+    public class DatabaseContext : DbContext
+    {
+        private readonly string _connectionString;
+        public DbSet<User> Users { get; set; }
+
+        public DatabaseContext(string connectionString)
+        {
+            _connectionString = connectionString;
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_connectionString);
+        }
+    }
+}
--- Data.Models/Models/User.cs
+++ Data.Models/Models/User.cs
@@ -1,15 +1,13 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
-using System.ComponentModel.DataAnnotations;
 
 namespace Data.Models.Models
 {
     public class User
     {
-        [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int UserId { get; set; }
-        public string Username { get; set; }
-        public string Password { get; set; }
-        public string Role { get; set; }
+        public int Id { get; set; }
+        public required string Username { get; set; }
+        public required string Password { get; set; }
+        public required string Role { get; set; }
     }
 }
